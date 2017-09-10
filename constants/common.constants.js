afb7740985dac9e28112548a554e844b96b34283
sergeialimov <sv.alimov@yandex.ru>
2017-09-10 17:17:37 +0300

    add file for constants

--- constants/common.constants.js
+++ constants/common.constants.js
@@ -1,6 +1,8 @@
 module.exports = Object.freeze({
   TITLE: {
-    homepage: 'Gmail'
+    homepage: 'Gmail',
+		inbox: 'Inbox - best.automation.test@gmail.com - Gmail'
+	
   },
   CREDENTIALS: {
     login: 'best.automation.test@gmail.com',
