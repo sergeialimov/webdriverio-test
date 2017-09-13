11d4272dc4305364d1653571776055c82400ffa8
sergeialimov <sv.alimov@yandex.ru>
2017-09-13 10:55:44 +0300

    add file for warning messages in different languages

---
+++ data/warning.data.js
@@ -0,0 +1,13 @@
+module.exports = Object.freeze({
+
+  ACCOUNT: {
+    'en': 'Couldn\'t find your Google Account',
+    'pl': 'Nie możemy znaleźć takiego konta Google',
+    'ru': 'Не удалось найти аккаунт Google'
+  },
+  PASSWORD: {
+    'en': 'Wrong password. Try again.',
+    'pl': 'Nieprawidłowe hasło. Spróbuj jeszcze raz.',
+    'ru': 'Неверный пароль. Повторите попытку.',
+  }
+});
